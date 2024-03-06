e24a25062c5a05cf97a3d5a57678bbbc08e27e91
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-03-06 23:46:10 +0700

    #1 - anhdev1104 update file handleAddProduct.js

--- handleAddProduct.js
+++ handleAddProduct.js
@@ -1,1 +1,11 @@
 console.log('products');
+class MyInfor {
+    constructor() {
+        this.name = 'Bùi Hoàng Anh';
+        this.dob = '01-01-2004';
+        this.gender = 'Nam';
+        this.address = 'Đà Nẵng, Việt Nam';
+        this.education = 'FPT Polytechnic';
+        this.position = 'Fullstack Deverloper';
+    }
+}
